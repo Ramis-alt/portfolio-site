cd9381547e68dd29a8867e6f9536b542e84b33d5
Ramiro Renteria <Ramis28@yahoo.com>
2024-07-20 11:05:26 -0700

    Cleaned up code

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,45 +1,4 @@
-// import React from 'react';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faGitSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-// import { Link } from 'react-router-dom';
-
-// export default function Info() {
-//     const afterClick = "https://www.linkedin.com/in/ramiro-renteria-24bba520/";
-//     const myGitHub = "https://github.com/Ramis-alt";
-//     return (
-//         <div className="main--body">
-//             <img src='images/ProfImage1_copy.png' alt="a headshot pic" />
-//             <div className="profile-info">
-//                 <h1>Ramiro Renteria</h1>
-//                 <h4>Bay Valley Tech Web Developer Intern</h4>
-//                 <h5><Link to='/contact' className='contact--edit'>Contact</Link></h5>
-//                 <hr></hr>
-//                 <div className="myDetails">
-//                     <h2>About</h2>
-//                     <p>Welcome to my website! I'm a passionate Full Stack developer with a deep fascination for web development.
-//                         I thrive on staying up-to-date with the latest technologies and best practices in the ever-evolving world of web development.
-//                         Exploring new horizons and embracing continuous learning are integral parts of my journey.
-//                         I'm excited to showcase my skills and share my experiences as I strive to create captivating and user-friendly web experiences.
-//                         Join me as I push the boundaries of what's possible on the web!
-//                     </p>
-//                     <h2>Interests</h2>
-//                     <p>New emerging technologies. Open to all kinds of music. Gamer. Outdoorsy. SciFi movie geek. 90's music. Traveling. Baseball.
-//                     </p>
-//                     <h2>Achievements</h2>
-//                     <ul className='cert-styling'>
-//                         <li><a href='/certificates/Ramiro Renteria-Student-ID-22324-Full Stack Web-App Development Course.pdf' target="_blank" rel="noopener noreferrer">Full Stack Cert</a></li>
-//                         <li><a href='/certificates/Ramiro Renteria-Student-ID-22324-Front End Web-App Development Course.pdf' target="_blank" rel="noopener noreferrer">Front End Cert</a></li>
-//                         <li><a href='/certificates/Ramiro Renteria-Student-ID-22324-Intro to Back-End Web-Development Module.pdf' target="_blank" rel="noopener noreferrer">Back End Cert</a></li>
-//                     </ul>
-//                 </div>
-//             </div>
-//         </div>
-//     )
-// }
-
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGitSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { Link } from 'react-router-dom';
 
 export default function Info() {
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import emailjs from 'emailjs-com'
 
 const Contact = () => {
