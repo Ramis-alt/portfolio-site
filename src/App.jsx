f204b13f0bb661d656f24ce3d2bf85d4e31e31f3
Ramiro Renteria <Ramis28@yahoo.com>
2024-08-12 22:53:01 -0700

    removed unnecessary files and added error handling component

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Footer from './components/Footer';
 import Info from './components/Info';
 import Contact from './pages/Contact';
+import ErrorBoundary from './components/ErrorBoundary';
 
 function App() {
-  const location = useLocation();
-
   return (
-    <Router basename="/portfolio-site">
-      <div className="container">
-        <Routes>
-          <Route path="/" element={<><Info /><Footer /></>} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </div>
-    </Router>
+    <ErrorBoundary>
+      <Router basename="/portfolio-site">
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<><Info /><Footer /></>} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
+      </Router>
+    </ErrorBoundary>
   );
 }
 
